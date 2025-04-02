5a32e11c420f07e14b4899d99b9530f4c9079133
Tuong Nguyen <yolotube21@yahoo.com>
2025-04-02 19:46:49 -0400

    Component creation

---
+++ src/components/greeting.jsx
@@ -0,0 +1,13 @@
+// Task 1
+import React from "react";
+
+function greetings() {
+    return (
+        <div>
+            <h2>Welcome to the greeting component</h2>
+            <p>This is your first custom component in React.</p>
+        </div>
+    )
+}
+export default greetings;
+// This is a simple React component that displays a greeting message.
